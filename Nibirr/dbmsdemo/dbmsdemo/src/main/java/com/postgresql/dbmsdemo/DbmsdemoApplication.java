5630840847b7ccba9d6e266aaf983dd975f77468
Nibir <zulqarnibir96@gmail.com>
2025-07-28 16:31:00 +0600

    Nibir commit

---
+++ Nibirr/dbmsdemo/dbmsdemo/src/main/java/com/postgresql/dbmsdemo/DbmsdemoApplication.java
@@ -0,0 +1,13 @@
+package com.postgresql.dbmsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsdemoApplication.class, args);
+	}
+
+}
